ad121022b956356926ffebf00612790be49f55cb
broberts2 <broc.roberts@outlook.com>
2020-10-24 21:54:07 -0600

    changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { createServer } = require("http");
 const config = require("./config");
+const http = serverConfig.production ? require("https") : require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
 const { createEventAdapter } = require("@slack/events-api");
@@ -23,7 +24,17 @@ app.use("/slack/events", slackEvents.expressMiddleware());
 
 app.use(bodyParser());
 
-const server = createServer(app);
+let server = null;
+
+if (serverConfig.production) {
+  const key = fs.readFileSync("/etc/letsencrypt/live/arclight.cc/privkey.pem");
+  const cert = fs.readFileSync("/etc/letsencrypt/live/arclight.cc/cert.pem");
+  const ca = fs.readFileSync("/etc/letsencrypt/live/arcligh.cc/chain.pem");
+  server = http.createServer({ key, cert, ca }, app);
+} else {
+  server = http.createServer(app);
+}
+
 server.listen(port, () => {
   console.log(`Listening for events on ${server.address().port}`);
 });
